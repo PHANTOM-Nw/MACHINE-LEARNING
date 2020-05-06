9ece2c0c80cd8819f3a3366e05af476ca1922d6d
PHANTOM-Nw <63948093+PHANTOM-Nw@users.noreply.github.com>
2020-05-06 20:25:30 +0800

    Update knn2.py

--- knn2.py
+++ knn2.py
@@ -1,5 +1,5 @@
-import numpy as np  # 导入numpy工具包
-from os import listdir  # 使用listdir模块，用于访问本地文件
+import numpy as np  # 导入numpy
+from os import listdir  # 使用listdir模块访问本地文件
 from sklearn import neighbors
 
 
@@ -15,13 +15,13 @@ def img2vector(fileName):
 
 def readDataSet(path):
     fileList = listdir(path)  # 获取文件夹下的所有文件
-    numFiles = len(fileList)  # 统计需要读取的文件的数目
-    dataSet = np.zeros([numFiles, 1024], int)  # 用于存放所有的数字文件
-    hwLabels = np.zeros([numFiles])  # 用于存放对应的标签(与神经网络的不同)
+    numFiles = len(fileList)  # 统计读取的文件数目
+    dataSet = np.zeros([numFiles, 1024], int)  # 存放所有的数字文件
+    hwLabels = np.zeros([numFiles])  # 存放标签
     for i in range(numFiles):  # 遍历所有的文件
         filePath = fileList[i]  # 获取文件名称/路径
         digit = int(filePath.split('_')[0])  # 通过文件名获取标签
-        hwLabels[i] = digit  # 直接存放数字，并非one-hot向量
+        hwLabels[i] = digit  
         dataSet[i] = img2vector(path + '/' + filePath)  # 读取文件内容
     return dataSet, hwLabels
 
@@ -34,8 +34,8 @@ knn.fit(train_dataSet, train_hwLabels)
 # read  testing dataSet
 dataSet, hwLabels = readDataSet('testDigits')
 
-res = knn.predict(dataSet)  # 对测试集进行预测
-error_num = np.sum(res != hwLabels)  # 统计分类错误的数目
+res = knn.predict(dataSet)  # 进行预测
+error_num = np.sum(res != hwLabels)  # 错误的数目
 num = len(dataSet)  # 测试集的数目
 print("Total num:", num, " Wrong num:", \
       error_num, "  WrongRate:", error_num / float(num))
